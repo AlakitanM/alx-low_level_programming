7dd80a2769e906a66077c23a2ffa1eab8aa82c45
AlakitanM <alakitanmariam800@gmail.com>
2023-04-23 03:36:19 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <atdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main (void)
+(
+        puts("programming is like building a multilingual puzzle\n");
+	return (0);
+)
